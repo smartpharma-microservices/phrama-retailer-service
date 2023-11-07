03b80dcc81cdc89834ef03f5d587812ccf7fe5d3
Panchakshari Venkata Sivakumar <panchaksharisiva@gmail.com>
2023-11-07 15:23:49 +0530

    Update RetailerClient.java

--- src/main/java/com/pharma/retailer/client/RetailerClient.java
+++ src/main/java/com/pharma/retailer/client/RetailerClient.java
@@ -3,6 +3,6 @@ package com.pharma.retailer.client;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(name="pharma-rms-inventory",url="http://localhost:8086",path="/api/v1/retailerInventory")
-public class RetailerClient {
+public interface RetailerClient {
 
 }
